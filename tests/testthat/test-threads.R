3fa0be8ef2d14f4edbcfb117b1904aa26ad5b2e0
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-10-19 08:59:29 +0200

    cleanup

--- tests/testthat/test-threads.R
+++ tests/testthat/test-threads.R
@@ -4,8 +4,6 @@ NOT_CRAN <-
   identical(Sys.getenv("TRAVIS"), "true")
 
 test_that("Setting STAN_NUM_THREADS", {
-  before_test_threads <- get_num_threads()
-
   set_num_threads(10)
   threads = as.numeric(Sys.getenv("STAN_NUM_THREADS"))
   expect_equal(threads, 10)
@@ -15,17 +13,10 @@ test_that("Setting STAN_NUM_THREADS", {
   threads = as.numeric(Sys.getenv("STAN_NUM_THREADS"))
   expect_equal(threads, 4)
   expect_equal(threads, get_num_threads())
-
-  #restore previous state
-  set_num_threads(before_test_threads)
 })
 
 test_that("Setting STAN_NUM_THREADS", {
-  before_test_threads <- get_num_threads()
   set_num_threads(10)
   expect_equal(get_num_threads(), 10)
   expect_equal(get_num_threads(), as.numeric(Sys.getenv("STAN_NUM_THREADS")))
-
-  #restore previous state
-  set_num_threads(before_test_threads)
 })
