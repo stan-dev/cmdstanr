7423ee860e4cf818aa83dc2f97eb2991cbac3847
Jonah Gabry <jgabry@gmail.com>
2019-10-19 23:51:11 -0400

    Update test-threads.R

--- tests/testthat/test-threads.R
+++ tests/testthat/test-threads.R
@@ -5,18 +5,18 @@ NOT_CRAN <-
 
 test_that("Setting STAN_NUM_THREADS", {
   set_num_threads(10)
-  threads = as.numeric(Sys.getenv("STAN_NUM_THREADS"))
+  threads = as.integer(Sys.getenv("STAN_NUM_THREADS"))
   expect_equal(threads, 10)
-  expect_equal(threads, get_num_threads())
+  expect_equal(threads, num_threads())
 
   set_num_threads(4)
-  threads = as.numeric(Sys.getenv("STAN_NUM_THREADS"))
+  threads = as.integer(Sys.getenv("STAN_NUM_THREADS"))
   expect_equal(threads, 4)
-  expect_equal(threads, get_num_threads())
+  expect_equal(threads, num_threads())
 })
 
 test_that("Setting STAN_NUM_THREADS", {
   set_num_threads(10)
-  expect_equal(get_num_threads(), 10)
-  expect_equal(get_num_threads(), as.numeric(Sys.getenv("STAN_NUM_THREADS")))
+  expect_equal(num_threads(), 10)
+  expect_equal(num_threads(), as.integer(Sys.getenv("STAN_NUM_THREADS")))
 })
