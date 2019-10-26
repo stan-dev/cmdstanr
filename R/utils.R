fb2accbb4edd739c919615dcb657d124b59eb802
jgabry <jgabry@gmail.com>
2019-10-26 18:33:59 -0400

    Clean up R code a little bit
    nothing strictly necessary, just a bit easier to read this way

--- R/utils.R
+++ R/utils.R
@@ -305,31 +305,29 @@ set_make_local <- function(threads = FALSE,
   cmdstanr_generated_flags_comment <- "# cmdstanr generated make/local flags (add user flags above this line)"
   make_local_path <- file.path(cmdstan_path(), "make", "local")
   user_flags <- c()
+  old_make_local_cmdstanr <- c()
   if (file.exists(make_local_path)) {
     old_make_local_all <- readLines(make_local_path)
-    old_make_local_cmdstanr <- c()
     is_user_flag <- TRUE
     for (x in old_make_local_all) {
       if (startsWith(x, cmdstanr_generated_flags_comment)) {
         is_user_flag <- FALSE
       }
-      if(!is_user_flag) {
+      if (!is_user_flag) {
         old_make_local_cmdstanr <- c(old_make_local_cmdstanr, x)
       } else {
         user_flags <- c(user_flags, x)
       }
     }
-  } else {
-    old_make_local_cmdstanr <- c()
   }
   old_make_local_cmdstanr <- paste(old_make_local_cmdstanr, collapse = "\n")
-  if(opencl) {
+  if (opencl) {
     stan_opencl <- "STAN_OPENCL = true"
-    platform_id <- paste("OPENCL_PLATFORM_ID", opencl_platform_id, sep = " = ")
-    device_id <- paste("OPENCL_DEVICE_ID", opencl_device_id, sep = " = ")
+    platform_id <- paste("OPENCL_PLATFORM_ID =", opencl_platform_id)
+    device_id <- paste("OPENCL_DEVICE_ID =", opencl_device_id)
     compiler_flags <- c(compiler_flags, stan_opencl, platform_id, device_id)
   }
-  if(threads) {
+  if (threads) {
     stan_threads <- "CXXFLAGS += -DSTAN_THREADS"
     compiler_flags <- c(compiler_flags, stan_threads)
   }
@@ -337,10 +335,10 @@ set_make_local <- function(threads = FALSE,
     compiler_flags <- c(cmdstanr_generated_flags_comment, compiler_flags)
   }
   new_make_local_cmdstanr <- paste(compiler_flags, collapse = "\n")
-  # dont rewrite make/local if there are no changes
   if (new_make_local_cmdstanr != old_make_local_cmdstanr) {
+    # only rewrite make/local if there are changes
     make_local_content <- c(user_flags, new_make_local_cmdstanr)
-    write(make_local_content, file = make_local_path, append = FALSE)
+    writeLines(make_local_content, make_local_path)
     return(TRUE)
   }
   return(FALSE)
