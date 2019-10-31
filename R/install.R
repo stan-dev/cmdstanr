a417a47c0265257228a29d9df6c7cab2b69cc2a3
jgabry <jgabry@gmail.com>
2019-10-31 17:28:14 -0400

    use repo_branch instead of branch_name for consistency

--- R/install.R
+++ R/install.R
@@ -3,7 +3,7 @@
 #' \if{html}{\figure{logo.png}{options: width="25px" alt="https://mc-stan.org/about/logo/"}}
 #' The `install_cmdstan()` function runs a script
 #' (see `inst/make_cmdstan.sh` on [GitHub](https://github.com/stan-dev/cmdstanr))
-#' that attempts to download and install the latest release of
+#' that by default attempts to download and install the latest release of
 #' [CmdStan](https://github.com/stan-dev/cmdstan/releases/latest). Currently the
 #' necessary C++ tool chain is assumed to be available (see Appendix B of the
 #' CmdStan [guide](https://github.com/stan-dev/cmdstan/releases/latest)),
@@ -19,12 +19,12 @@
 #' @param quiet Defaults to `FALSE`, but if `TRUE` will suppress the verbose
 #'   output during the installation process.
 #' @param overwrite Defaults to `FALSE`, but if `TRUE` will download and
-#'   reinstall CmdStan even if an existing installation was found.
+#'   reinstall CmdStan even if an existing installation is found.
 #' @param repo_clone Defaults to `FALSE`, but if `TRUE` will install a
-#'   git clone of cmdstan and checkout the specified branch.
+#'   git clone of CmdStan and checkout the specified branch.
 #' @param repo_url The URL of the git repository to clone. The default URL
-#'   is the stan-dev cmdstan github repository
-#' @param repo_branch The branch to checkout in the cloned repository
+#'   points to the `stan-dev/cmdstan` github repository.
+#' @param repo_branch The branch to checkout in the cloned repository.
 #'
 #' @return [Invisibly][base::invisible], the list returned by [processx::run()],
 #'   which contains information about the system process that was run. See the
@@ -89,27 +89,27 @@ install_cmdstan <- function(dir = NULL,
   invisible(install_log)
 }
 
-#' Checks out a branch in the `stan-dev/cmdstan` git repository
+#' Check out a branch from a git repository and rebuild CmdStan
 #'
-#' This only works if the installed CmdStan is a clone of a git repository.
+#' This only works if the installed CmdStan is from a clone of the git
+#' repository.
 #'
 #' @export
-#' @param branch_name Name of the git repository branch to checkout.
-#' @param clean_and_rebuild If `TRUE` (the default), will run `make clean-all`
-#'   and `make build` after cheking out the new branch.
 #' @inheritParams install_cmdstan
 #' @inherit install_cmdstan return
+#' @param clean_and_rebuild If `TRUE` (the default), will run `make clean-all`
+#'   and `make build` after checking out the new branch.
 #'
 #' @seealso [install_cmdstan()]
 #'
-cmdstan_git_checkout_branch <- function(branch_name,
+cmdstan_git_checkout_branch <- function(repo_branch,
                                         clean_and_rebuild = TRUE,
                                         cores = 2,
                                         quiet = FALSE) {
   make_cmdstan <- system.file("make_cmdstan.sh", package = "cmdstanr")
   make_cmdstan <- c(make_cmdstan, paste0("-d ", cmdstan_path()))
   make_cmdstan <- c(make_cmdstan, paste0("-j", cores))
-  make_cmdstan <- c(make_cmdstan, "-c", "-b", branch_name)
+  make_cmdstan <- c(make_cmdstan, "-c", "-b", repo_branch)
   install_log <- processx::run(
     command = "bash",
     args = make_cmdstan,
