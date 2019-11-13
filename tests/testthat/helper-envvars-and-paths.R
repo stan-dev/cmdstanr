c57d62ec9c4c780f43b6da694efc8e1b1496658a
jgabry <jgabry@gmail.com>
2019-11-13 15:02:24 -0500

    cleanup helper file

--- tests/testthat/helper-envvars-and-paths.R
+++ tests/testthat/helper-envvars-and-paths.R
@@ -1,14 +1,18 @@
 
-not_on_cran <- function() {
-  identical(Sys.getenv("NOT_CRAN"), "true")
-}
-
 on_appveyor <- function() {
   identical(tolower(Sys.getenv("APPVEYOR")), "true")
 }
 
+on_travis <- function() {
+  identical(Sys.getenv("TRAVIS"), "true")
+}
+
 on_codecov <- function() {
   identical(Sys.getenv("R_COVR"), "true")
+}
+
+not_on_cran <- function() {
+  on_travis() || on_appveyor() || identical(Sys.getenv("NOT_CRAN"), "true")
 }
 
 set_cmdstan_path_for_tests <- function() {
