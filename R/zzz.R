2a501ce85740966b44afa2364413226376b38f41
jgabry <jgabry@gmail.com>
2019-11-14 23:29:09 -0500

    Update zzz.R
    [ci skip]

--- R/zzz.R
+++ R/zzz.R
@@ -3,11 +3,11 @@
   packageStartupMessage("This is cmdstanr version ", ver)
   packageStartupMessage("- Online documentation and vignettes at mc-stan.org/cmdstanr")
   if (is.null(.cmdstanr$PATH)) {
-    packageStartupMessage("- Use set_cmdstan_path() to set the path to CmdStan.")
-    packageStartupMessage("- Use install_cmdstan() to install CmdStan.")
+    packageStartupMessage("- Use set_cmdstan_path() to set the path to CmdStan")
+    packageStartupMessage("- Use install_cmdstan() to install CmdStan")
   } else {
     packageStartupMessage("- CmdStan path set to: ", cmdstan_path(), "")
-    packageStartupMessage("- Use set_cmdstan_path() to change the path.")
+    packageStartupMessage("- Use set_cmdstan_path() to change the path")
   }
 }
 
