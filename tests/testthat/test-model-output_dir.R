208ffcc6003c5f0d37f44a608926a9a209fdee82
jgabry <jgabry@gmail.com>
2019-11-18 22:41:47 -0500

    create dir in temporary directory in tests

--- tests/testthat/test-model-output_dir.R
+++ tests/testthat/test-model-output_dir.R
@@ -2,12 +2,16 @@ context("model-output_dir")
 
 if (not_on_cran()) {
   set_cmdstan_path()
+  sandbox <- file.path(tempdir(check = TRUE), "sandbox")
+  if (!dir.exists(sandbox)) {
+    dir.create(sandbox)
+  }
 }
 
 test_that("all fitting methods work with output_dir", {
   skip_on_cran()
   for (method in c("sample", "optimize", "variational")) {
-    method_dir <- test_path("answers", "sandbox", method)
+    method_dir <- file.path(sandbox, method)
     if (!dir.exists(method_dir)) {
       dir.create(method_dir)
     }
@@ -25,10 +29,10 @@ test_that("all fitting methods work with output_dir", {
 
   # specifying output_dir and save_diagnostics
   fit <- testing_fit("bernoulli", method = "sample", seed = 123,
-                     output_dir = test_path("answers", "sandbox", "sample"),
+                     output_dir = file.path(sandbox, "sample"),
                      save_diagnostics = TRUE)
 
-  files <- list.files(test_path("answers", "sandbox", "sample"))
+  files <- list.files(file.path(sandbox, "sample"))
   expect_equal(
     sum(grepl("diagnostic", files)),
     fit$num_runs()
@@ -48,7 +52,7 @@ test_that("error if output_dir is invalid", {
     "No directory provided"
   )
 
-  not_readable <- test_path("answers", "sandbox", "locked")
+  not_readable <- file.path(sandbox, "locked")
   dir.create(not_readable, mode = 0)
   expect_error(
     testing_fit("bernoulli", output_dir = not_readable),
