8fadef46d82259c3bda1ba2de14e8b3d44fc39ce
jgabry <jgabry@gmail.com>
2019-11-19 21:47:43 -0500

    try removing skip_on_covr from install tests

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -2,13 +2,9 @@ context("install")
 
 test_that("test cmdstan installation", {
   skip_if_offline()
-  skip_on_covr()
+  # skip_on_covr()
 
-  if (not_on_cran()) {
-    dir <- dirname(cmdstan_default_path())
-  } else {
-    dir <- tempdir()
-  }
+  dir <- tempdir(check = TRUE)
   expect_message(
     expect_output(
       install_cmdstan(dir = dir, cores = 2, quiet = FALSE, overwrite = TRUE),
