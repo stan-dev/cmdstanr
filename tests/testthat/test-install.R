905218dd1273a968e7d1ac22e5a82ae29f98e3e7
jgabry <jgabry@gmail.com>
2019-11-20 13:26:05 -0500

    add test for timeout argument

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -31,10 +31,40 @@ test_that("install_cmdstan() errors if installation already exists", {
   }
 })
 
-test_that("internal clone_repo() function clones the repo", {
-  skip_on_covr() 
-  skip_on_cran()
+test_that("install_cmdstan() errors if it times out", {
   skip_if_offline()
+  dir <- tempdir(check = TRUE)
+
+  dir_exists <- dir.exists(file.path(dir, "cmdstan"))
+
+  # with quiet=TRUE
+  expect_warning(
+    expect_message(
+      install_cmdstan(dir = dir, timeout = 1, quiet = TRUE, overwrite = dir_exists),
+      if (dir_exists) "* Removing the existing installation" else "* Latest CmdStan release",
+      fixed = TRUE
+    ),
+    "increasing the value of the 'timeout' argument and running again with 'quiet=FALSE'",
+    fixed = TRUE
+  )
+
+  # with quiet=FALSE
+  expect_warning(
+    expect_message(
+      install_cmdstan(dir = dir, timeout = 1, quiet = FALSE, overwrite = dir_exists),
+      if (dir_exists) "* Removing the existing installation" else "* Latest CmdStan release",
+      fixed = TRUE
+    ),
+    "Try increasing the value of the 'timeout' argument.",
+    fixed = TRUE
+  )
+})
+
+test_that("internal clone_repo() function clones the repo", {
+  skip_if_offline()
+  skip_on_cran()
+  skip_on_covr()
+
   clone_dir <- tempfile(tmpdir = tempdir(check=TRUE))
   if (!dir.exists(clone_dir)) {
     dir.create(clone_dir)
@@ -50,5 +80,4 @@ test_that("internal clone_repo() function clones the repo", {
   checkmate::expect_directory_exists(file.path(clone_dir, "stan"))
   checkmate::expect_directory_exists(file.path(clone_dir, "stan", "lib", "stan_math"))
 })
-
 
