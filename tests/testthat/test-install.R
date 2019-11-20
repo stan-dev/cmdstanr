ff4bae9d1d03d414d3928bf8bcb0e1efc84b6c4f
jgabry <jgabry@gmail.com>
2019-11-20 13:23:43 -0500

    Fix test that was failing
    don't use `NULL` in file.path

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -16,11 +16,14 @@ test_that("install_cmdstan() successfully installs cmdstan", {
 test_that("install_cmdstan() errors if installation already exists", {
   skip_if_offline()
   if (not_on_cran()) {
+    # want to test passing NULL to install_cmdstan but need a real dir to
+    # check in dir.exists() below so also create dir_check
     dir <- NULL
+    dir_check <- cmdstan_default_path()
   } else {
-    dir <- tempdir()
+    dir <- dir_check <- tempdir()
   }
-  if (dir.exists(file.path(dir, "cmdstan"))) {
+  if (dir.exists(file.path(dir_check, "cmdstan"))) {
     expect_warning(
       install_cmdstan(dir = dir, overwrite = FALSE),
       "An installation already exists"
