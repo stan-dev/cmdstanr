e038e634d42c9241b02ad6a223125e9f417c0193
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-11-24 19:28:18 +0100

    add check functions

--- R/fit.R
+++ R/fit.R
@@ -234,6 +234,12 @@ CmdStanMCMC <- R6::R6Class(
   classname = "CmdStanMCMC",
   inherit = CmdStanFit,
   public = list(
+    initialize = function(runset) {
+      super$initialize(runset)
+      data_csv <- read_sample_csv(self$output_files())
+      check_divergences(data_csv)
+      check_sampler_transitions_treedepth(data_csv)
+    },
     num_chains = function() {
       super$num_runs()
     },
@@ -254,6 +260,8 @@ CmdStanMCMC <- R6::R6Class(
              call. = FALSE)
       }
       data_csv <- read_sample_csv(self$output_files())
+      check_divergences(data_csv)
+      check_sampler_transitions_treedepth(data_csv)
       private$draws_ <- data_csv$post_warmup
       private$warmup_draws_ <- data_csv$warmup
       private$sampler_params_ <- data_csv$sampler
