7a09f8f45a275326bbec927306373d7d21a43023
Ben <bbbales2@gmail.com>
2019-12-07 14:00:03 -0500

    Added checks to make sure divergences/max treedepths warnings don't happen in warmup
    Fixed typos in csv tests where it wasn't testing warmup but was supposed to (Issue #98)

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,6 +6,11 @@ test_that("check_divergences() works", {
   csv_output <- read_sample_csv(csv_files)
   output <- "14 of 100 \\(14%\\) transitions ended with a divergence."
   expect_message(check_divergences(csv_output), output)
+
+  csv_files <- c(test_path("resources", "csv", "model1-2-warmup.csv"))
+  csv_output <- read_sample_csv(csv_files)
+  output <- "1 of 100 \\(1%\\) transitions ended with a divergence."
+  expect_message(check_divergences(csv_output), output)
 })
 
 test_that("check_sampler_transitions_treedepth() works", {
@@ -13,5 +18,10 @@ test_that("check_sampler_transitions_treedepth() works", {
   csv_files <- c(test_path("resources", "csv", "model1-2-no-warmup.csv"))
   csv_output <- read_sample_csv(csv_files)
   output <- "16 of 100 \\(16%\\) transitions hit the maximum treedepth limit of 5 or 2\\^5-1 leapfrog steps."
+  expect_message(check_sampler_transitions_treedepth(csv_output), output)
+
+  csv_files <- c(test_path("resources", "csv", "model1-2-warmup.csv"))
+  csv_output <- read_sample_csv(csv_files)
+  output <- "1 of 100 \\(1%\\) transitions hit the maximum treedepth limit of 5 or 2\\^5-1 leapfrog steps."
   expect_message(check_sampler_transitions_treedepth(csv_output), output)
 })
