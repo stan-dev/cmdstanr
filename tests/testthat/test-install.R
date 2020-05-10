96ddd02e7766dd8ff5eb94f81af9546a4fe40592
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-05-10 12:07:30 +0200

    fix tests

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -19,20 +19,24 @@ test_that("install_cmdstan() successfully installs cmdstan", {
 
 test_that("install_cmdstan() errors if installation already exists", {
   skip_if_offline()
+  ver <- latest_released_version()
   if (not_on_cran()) {
     # want to test passing NULL to install_cmdstan but need a real dir to
     # check in dir.exists() below so also create dir_check
-    dir <- NULL
-    dir_check <- cmdstan_default_path()
+    install_dir <- cmdstan_default_install_path()
   } else {
-    dir <- dir_check <- tempdir()
+    install_dir <- tempdir()
   }
-  if (dir.exists(file.path(dir_check, "cmdstan"))) {
-    expect_warning(
-      install_cmdstan(dir = dir, overwrite = FALSE),
-      "An installation already exists"
-    )
+  dir <- file.path(install_dir, paste0("cmdstan-",ver))
+  fake_folder <- FALSE
+  if (!dir.exists(dir)) {
+    fake_folder <- TRUE
+    dir.create(dir)
   }
+  expect_warning(
+    install_cmdstan(dir = install_dir, overwrite = FALSE),
+    "An installation already exists"
+  )
 })
 
 test_that("install_cmdstan() errors if it times out", {
@@ -42,8 +46,8 @@ test_that("install_cmdstan() errors if it times out", {
   } else {
     dir <- tempdir(check = TRUE)
   }
-  dir_exists <- dir.exists(file.path(dir, "cmdstan"))
-
+  ver <- latest_released_version()
+  dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
   # with quiet=TRUE
   expect_warning(
     expect_message(
@@ -54,7 +58,7 @@ test_that("install_cmdstan() errors if it times out", {
     "increasing the value of the 'timeout' argument and running again with 'quiet=FALSE'",
     fixed = TRUE
   )
-
+  dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
   # with quiet=FALSE
   expect_warning(
     expect_message(
