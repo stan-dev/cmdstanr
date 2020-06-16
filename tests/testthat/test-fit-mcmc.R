f36c65c870e9e8f4c468f66a167a87c1f60b4a9c
jgabry <jgabry@gmail.com>
2020-06-15 22:27:03 -0400

    add tests for inverse_metric()

--- tests/testthat/test-fit-mcmc.R
+++ tests/testthat/test-fit-mcmc.R
@@ -13,7 +13,7 @@ if (not_on_cran()) {
   fit_mcmc_2 <- testing_fit("logistic", method = "sample",
                             seed = 123, chains = 1,
                             iter_sampling = 100000,
-                            refresh = 0)
+                            refresh = 0, metric = "dense_e")
   PARAM_NAMES <- c("alpha", "beta[1]", "beta[2]", "beta[3]")
 }
 
@@ -56,6 +56,25 @@ test_that("draws() method returns draws_array (reading csv works)", {
   expect_s3_class(draws_all_after, "draws_array")
   expect_equal(posterior::nvariables(draws_all_after), 5)
   expect_equal(posterior::nchains(draws_all_after), fit_mcmc$num_chains())
+})
+
+test_that("inverse_metric method works after mcmc", {
+  x <- fit_mcmc_1$inverse_metric()
+  expect_length(x, fit_mcmc_1$num_chains())
+  checkmate::expect_matrix(x[[1]])
+  checkmate::expect_matrix(x[[2]])
+  expect_equal(x[[1]], diag(diag(x[[1]])))
+
+  x <- fit_mcmc_1$inverse_metric(matrix=FALSE)
+  expect_length(x, fit_mcmc_1$num_chains())
+  expect_null(dim(x[[1]]))
+  checkmate::expect_numeric(x[[1]])
+  checkmate::expect_numeric(x[[2]])
+
+  x <- fit_mcmc_2$inverse_metric()
+  expect_length(x, fit_mcmc_2$num_chains())
+  checkmate::expect_matrix(x[[1]])
+  expect_false(x[[1]][1,2] == 0) # dense
 })
 
 test_that("summary() method works after mcmc", {
