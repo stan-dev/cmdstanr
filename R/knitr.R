70c4acb0bc36aeac89466097b79cce9ff055b514
Mikhail Popov <mikhail@mpopov.com>
2020-08-01 20:00:36 -0400

    Stan knitr engine: iterations based on code review

--- R/knitr.R
+++ R/knitr.R
@@ -1,6 +1,7 @@
 #' Register the knitr engine for Stan
 #'
 #' Registers CmdStanR's knitr engine [eng_stan()] for processing Stan chunks.
+#' Refer to the vignette "R Markdown Stan Engine" for a demonstration.
 #'
 #' @param override Override knitr's built-in, RStan-based engine for `stan`.
 #'   See below for details.
@@ -10,17 +11,20 @@
 #' `override = FALSE`, this registers a `cmdstan` engine so that both engines
 #' may be used in the same R Markdown document. If there is syntax highlighting,
 #' the `cmdstan` chunks will have `stan` syntax highlighting applied to them.
+#'
+#' See the vignette "R Markdown Stan Engine" for an example.
 #' @references
 #' - [Register a custom language engine](https://bookdown.org/yihui/rmarkdown-cookbook/custom-engine.html)
 #' - [Stan language engine](https://bookdown.org/yihui/rmarkdown/language-engines.html#stan)
 #' @export
 register_knitr_engine <- function(override = TRUE) {
-  if (requireNamespace("knitr", quietly = TRUE)) {
-    if (override) {
-      knitr::knit_engines$set(stan = eng_stan)
-    } else {
-      knitr::knit_engines$set(cmdstan = eng_stan)
-    }
+  if (!requireNamespace("knitr", quietly = TRUE)) {
+    stop("please install the knitr package")
+  }
+  if (override) {
+    knitr::knit_engines$set(stan = eng_stan)
+  } else {
+    knitr::knit_engines$set(cmdstan = eng_stan)
   }
 }
 
@@ -28,27 +32,38 @@ register_knitr_engine <- function(override = TRUE) {
 #'
 #' This provides a `stan` engine for `knitr`, suitable for usage when attempting
 #' to render Stan chunks and compile the model code within to an executable with
-#' CmdStan.
+#' CmdStan. Use [register_knitr_engine()] to make this the default engine for
+#' `stan` chunks.
 #'
 #' @param options Chunk options, as provided by `knitr` during chunk execution
+#' @examples \dontrun{
+#' knitr::knit_engines$set(stan = cmdstanr::eng_stan)
+#' }
 #' @export
 eng_stan <- function(options) {
   output_var <- options$output.var
   if (!is.character(output_var) || length(output_var) != 1L)
     stop(
       "the chunk option output.var must be a character string ",
-      "providing a name for the returned `CmdStanModel` object."
+      "providing a name for the returned `CmdStanModel` object"
     )
   if (options$eval) {
     if (options$cache)
-      dir <- knitr:::valid_path(options[["cache.path"]], options$label)
+      dir <- knitr_valid_path(options[["cache.path"]], options$label)
     else
       dir <- tempdir()
     model_file <- write_stan_tempfile(options$code, dir)
     csm <- cmdstan_model(model_file)
     assign(output_var, csm, envir = knitr::knit_global())
   }
-  options$engine <- "stan"
-  code <- knitr:::one_string(options$code)
+  options$engine <- "stan" # for syntax highlighting
+  code <- paste(options$code, collapse = "\n")
   knitr::engine_output(options, code, '')
+}
+
+# Since {knitr} does not export valid_path():
+knitr_valid_path <- function(prefix, label) {
+  if (length(prefix) == 0L || is.na(prefix) || prefix == "NA")
+    prefix <- ""
+  return(paste0(prefix, label))
 }
