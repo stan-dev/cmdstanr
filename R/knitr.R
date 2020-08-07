35eb642008466a77741cf379b2f75573f645cefd
Mikhail Popov <mikhail@mpopov.com>
2020-08-06 22:12:45 -0400

    Notes on running rmd chunks interactively

--- R/knitr.R
+++ R/knitr.R
@@ -11,13 +11,25 @@
 #' If `override = TRUE` (default), this registers CmdStanR's knitr engine as the
 #' engine for `stan` chunks, replacing knitr's built-in, RStan-based engine. If
 #' `override = FALSE`, this registers a `cmdstan` engine so that both engines
-#' may be used in the same R Markdown document. If there is syntax highlighting,
-#' the `cmdstan` chunks will have `stan` syntax highlighting applied to them.
+#' may be used in the same R Markdown document. If the template supports syntax
+#' highlighting for the Stan language, the `cmdstan` chunks will have `stan`
+#' syntax highlighting applied to them.
 #'
 #' See the vignette
 #' [R Markdown CmdStan Engine](https://mc-stan.org/cmdstanr/articles/r-markdown.html)
 #' for an example.
 #'
+#' **Note:** When running chunks interactively in RStudio (e.g. when using
+#' [R Notebooks](https://bookdown.org/yihui/rmarkdown/notebook.html)), it has
+#' been observed that the built-in, RStan-based engine is used for `stan`
+#' chunks even when CmdStanR's engine has been registered in the session. When
+#' the R Markdown document is knit/rendered, the correct engine is used. As a
+#' workaround, when running chunks interactively, it is recommended to use the
+#' `override = FALSE` option and change `stan` chunks to be `cmdstan` chunks.
+#'
+#' If you would like to keep `stan` chunks as `stan` chunks, it is possible to
+#' specify `engine = "cmdstan"` in the chunk options after registering the
+#' `cmdstan` engine with `override = FALSE`.
 #' @references
 #' - [Register a custom language engine](https://bookdown.org/yihui/rmarkdown-cookbook/custom-engine.html)
 #' - [Stan language engine](https://bookdown.org/yihui/rmarkdown/language-engines.html#stan)
