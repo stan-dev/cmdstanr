0a733a4da4b3dacb31e9b4fb3f3067c765e3240e
Rok Češnovar <rok.cesnovar@gmail.com>
2020-10-24 14:28:52 +0200

    tests for Rtools 3.5

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -1,159 +1,180 @@
 context("install")
 
-# test_that("install_cmdstan() successfully installs cmdstan", {
-#   skip_if_offline()
-#   if (os_is_windows()) skip_on_covr()
-#   if (getRversion() < '3.5.0') {
-#     dir <- tempdir()
-#   } else {
-#     dir <- tempdir(check = TRUE)
-#   }
-#   expect_message(
-#     expect_output(
-#       install_cmdstan(dir = dir, cores = 2, quiet = FALSE, overwrite = TRUE,
-#                       release_url = test_release_url()),
-#       "Compiling, linking C++ code",
-#       fixed = TRUE
-#     ),
-#     "CmdStan path set"
-#   )
-# })
-#
-# test_that("install_cmdstan() errors if installation already exists", {
-#   skip_if_offline()
-#   if (os_is_windows()) skip_on_covr()
-#   if (not_on_cran()) {
-#     # want to test passing NULL to install_cmdstan but need a real dir to
-#     # check in dir.exists() below so also create dir_check
-#     install_dir <- cmdstan_default_install_path()
-#   } else {
-#     install_dir <- tempdir()
-#   }
-#   dir <- file.path(install_dir, "cmdstan-2.23.0")
-#   fake_folder <- FALSE
-#   if (!dir.exists(dir)) {
-#     fake_folder <- TRUE
-#     dir.create(dir)
-#   }
-#   expect_warning(
-#     install_cmdstan(dir = install_dir, overwrite = FALSE,
-#                     release_url = test_release_url()),
-#     "An installation already exists"
-#   )
-# })
-#
-# test_that("install_cmdstan() errors if it times out", {
-#   skip_if_offline()
-#   if (os_is_windows()) skip_on_covr()
-#   if (getRversion() < '3.5.0') {
-#     dir <- tempdir()
-#   } else {
-#     dir <- tempdir(check = TRUE)
-#   }
-#   ver <- latest_released_version()
-#   dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
-#   # with quiet=TRUE
-#   expect_warning(
-#     expect_message(
-#       install_cmdstan(dir = dir, timeout = 1, quiet = TRUE, overwrite = dir_exists,
-#                       release_url = test_release_url()),
-#       if (dir_exists) "* Removing the existing installation" else "* * Installing Cmdstan from https://github.com",
-#       fixed = TRUE
-#     ),
-#     "increasing the value of the 'timeout' argument and running again with 'quiet=FALSE'",
-#     fixed = TRUE
-#   )
-#   dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
-#   # with quiet=FALSE
-#   expect_warning(
-#     expect_message(
-#       install_cmdstan(dir = dir, timeout = 1, quiet = FALSE, overwrite = dir_exists,
-#                       release_url = test_release_url()),
-#       if (dir_exists) "* Removing the existing installation" else "* * Installing Cmdstan from https://github.com",
-#       fixed = TRUE
-#     ),
-#     "Try increasing the value of the 'timeout' argument.",
-#     fixed = TRUE
-#   )
-# })
-#
-# test_that("install_cmdstan() errors if invalid version or URL", {
-#   skip_if_offline()
-#   expect_error(
-#     install_cmdstan(version = "2.23.2"),
-#     "Download of Cmdstan failed. Please check if the supplied version number is valid."
-#   )
-#   expect_error(
-#     install_cmdstan(release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.23.2/cmdstan-2.23.2.tar.gz"),
-#     "Download of Cmdstan failed. Please check if the supplied release URL is valid."
-#   )
-# })
-#
-# test_that("install_cmdstan() works with version and release_url", {
-#   skip_if_offline()
-#   if (os_is_windows()) skip_on_covr()
-#   if (getRversion() < '3.5.0') {
-#     dir <- tempdir()
-#   } else {
-#     dir <- tempdir(check = TRUE)
-#   }
-#   expect_message(
-#     expect_output(
-#       install_cmdstan(dir = dir, overwrite = TRUE, cores = 4,
-#                       release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.24.0/cmdstan-2.24.0.tar.gz"),
-#       "Compiling, linking C++ code",
-#       fixed = TRUE
-#     ),
-#     "Finished installing CmdStan"
-#   )
-#   expect_warning(
-#     expect_message(
-#       expect_output(
-#         install_cmdstan(dir = dir, overwrite = TRUE, cores = 4,
-#                         version = "2.23.0",
-#                         # the URL is intentionally invalid to test that the version has higher priority
-#                         release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.23.2/cmdstan-2.23.2.tar.gz"),
-#         "Compiling, linking C++ code",
-#         fixed = TRUE
-#       ),
-#       "Finished installing CmdStan"
-#     ),
-#     "version and release_url are supplied to install_cmdstan()"
-#   )
-#   expect_true(dir.exists(file.path(dir, "cmdstan-2.23.0")))
-# })
-#
-# test_that("toolchain checks on Unix work", {
-#   skip_if(os_is_windows())
-#   path_backup <- Sys.getenv("PATH")
-#   Sys.setenv("PATH" = "")
-#   expect_error(
-#     check_unix_cpp_compiler(),
-#     "A C++ compiler was not found. Please install the 'clang++' or 'g++' compiler, restart R, and run check_cmdstan_toolchain().",
-#     fixed = TRUE
-#   )
-#   expect_error(
-#     check_unix_make(),
-#     "The 'make' tool was not found. Please install 'make', restart R, and then run check_cmdstan_toolchain().",
-#     fixed = TRUE
-#   )
-#   Sys.setenv("PATH" = path_backup)
-# })
-#
-# test_that("toolchain checks on Windows with RTools 3.5 work", {
-#   skip_if_not(os_is_windows())
-#   path_backup <- Sys.getenv("PATH")
-#   Sys.setenv("PATH" = "")
-#   expect_error(
-#     check_rtools35_windows_toolchain(),
-#     "\nA toolchain was not found. Please install RTools 3.5 and run",
-#     fixed = TRUE
-#   )
-#   Sys.setenv("PATH" = path_backup)
-# })
+test_that("install_cmdstan() successfully installs cmdstan", {
+  skip_if_offline()
+  if (os_is_windows()) skip_on_covr()
+  if (getRversion() < '3.5.0') {
+    dir <- tempdir()
+  } else {
+    dir <- tempdir(check = TRUE)
+  }
+  expect_message(
+    expect_output(
+      install_cmdstan(dir = dir, cores = 2, quiet = FALSE, overwrite = TRUE,
+                      release_url = test_release_url()),
+      "Compiling, linking C++ code",
+      fixed = TRUE
+    ),
+    "CmdStan path set"
+  )
+})
+
+test_that("install_cmdstan() errors if installation already exists", {
+  skip_if_offline()
+  if (os_is_windows()) skip_on_covr()
+  if (not_on_cran()) {
+    # want to test passing NULL to install_cmdstan but need a real dir to
+    # check in dir.exists() below so also create dir_check
+    install_dir <- cmdstan_default_install_path()
+  } else {
+    install_dir <- tempdir()
+  }
+  dir <- file.path(install_dir, "cmdstan-2.23.0")
+  fake_folder <- FALSE
+  if (!dir.exists(dir)) {
+    fake_folder <- TRUE
+    dir.create(dir)
+  }
+  expect_warning(
+    install_cmdstan(dir = install_dir, overwrite = FALSE,
+                    release_url = test_release_url()),
+    "An installation already exists"
+  )
+})
+
+test_that("install_cmdstan() errors if it times out", {
+  skip_if_offline()
+  if (os_is_windows()) skip_on_covr()
+  if (getRversion() < '3.5.0') {
+    dir <- tempdir()
+  } else {
+    dir <- tempdir(check = TRUE)
+  }
+  ver <- latest_released_version()
+  dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
+  # with quiet=TRUE
+  expect_warning(
+    expect_message(
+      install_cmdstan(dir = dir, timeout = 1, quiet = TRUE, overwrite = dir_exists,
+                      release_url = test_release_url()),
+      if (dir_exists) "* Removing the existing installation" else "* * Installing Cmdstan from https://github.com",
+      fixed = TRUE
+    ),
+    "increasing the value of the 'timeout' argument and running again with 'quiet=FALSE'",
+    fixed = TRUE
+  )
+  dir_exists <- dir.exists(file.path(dir, paste0("cmdstan-",ver)))
+  # with quiet=FALSE
+  expect_warning(
+    expect_message(
+      install_cmdstan(dir = dir, timeout = 1, quiet = FALSE, overwrite = dir_exists,
+                      release_url = test_release_url()),
+      if (dir_exists) "* Removing the existing installation" else "* * Installing Cmdstan from https://github.com",
+      fixed = TRUE
+    ),
+    "Try increasing the value of the 'timeout' argument.",
+    fixed = TRUE
+  )
+})
+
+test_that("install_cmdstan() errors if invalid version or URL", {
+  skip_if_offline()
+  expect_error(
+    install_cmdstan(version = "2.23.2"),
+    "Download of Cmdstan failed. Please check if the supplied version number is valid."
+  )
+  expect_error(
+    install_cmdstan(release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.23.2/cmdstan-2.23.2.tar.gz"),
+    "Download of Cmdstan failed. Please check if the supplied release URL is valid."
+  )
+})
+
+test_that("install_cmdstan() works with version and release_url", {
+  skip_if_offline()
+  if (os_is_windows()) skip_on_covr()
+  if (getRversion() < '3.5.0') {
+    dir <- tempdir()
+  } else {
+    dir <- tempdir(check = TRUE)
+  }
+  expect_message(
+    expect_output(
+      install_cmdstan(dir = dir, overwrite = TRUE, cores = 4,
+                      release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.24.0/cmdstan-2.24.0.tar.gz"),
+      "Compiling, linking C++ code",
+      fixed = TRUE
+    ),
+    "Finished installing CmdStan"
+  )
+  expect_warning(
+    expect_message(
+      expect_output(
+        install_cmdstan(dir = dir, overwrite = TRUE, cores = 4,
+                        version = "2.23.0",
+                        # the URL is intentionally invalid to test that the version has higher priority
+                        release_url = "https://github.com/stan-dev/cmdstan/releases/download/v2.23.2/cmdstan-2.23.2.tar.gz"),
+        "Compiling, linking C++ code",
+        fixed = TRUE
+      ),
+      "Finished installing CmdStan"
+    ),
+    "version and release_url are supplied to install_cmdstan()"
+  )
+  expect_true(dir.exists(file.path(dir, "cmdstan-2.23.0")))
+})
+
+test_that("toolchain checks on Unix work", {
+  skip_if(os_is_windows())
+  path_backup <- Sys.getenv("PATH")
+  Sys.setenv("PATH" = "")
+  expect_error(
+    check_unix_cpp_compiler(),
+    "A C++ compiler was not found. Please install the 'clang++' or 'g++' compiler, restart R, and run check_cmdstan_toolchain().",
+    fixed = TRUE
+  )
+  expect_error(
+    check_unix_make(),
+    "The 'make' tool was not found. Please install 'make', restart R, and then run check_cmdstan_toolchain().",
+    fixed = TRUE
+  )
+  Sys.setenv("PATH" = path_backup)
+})
+
+test_that("toolchain checks on Windows with RTools 3.5 work", {
+  skip_if_not(os_is_windows())
+  skip_if(R.Version()$major > "3")
+
+  path_backup <- Sys.getenv("PATH")
+  Sys.setenv("PATH" = "")
+  tmpdir <- tempdir()
+  tmp_dir1 <- file.path(tmpdir, "dir1")
+  tmp_dir2 <- file.path(tmpdir, "dir2")
+  if (dir.exists(tmp_dir1)) unlink(tmp_dir1)
+  if (dir.exists(tmp_dir2)) unlink(tmp_dir2)
+  expect_error(
+    check_rtools35_windows_toolchain(paths= c(tmp_dir1, tmp_dir2)),
+    "\nA toolchain was not found. Please install RTools 3.5 and run",
+    fixed = TRUE
+  )
+  if (!dir.exists(tmp_dir1)) dir.create(tmp_dir1)
+  expect_error(
+    check_rtools35_windows_toolchain(paths= c(tmp_dir1, tmp_dir2)),
+    "\nRTools installation found but PATH was not properly set.",
+    fixed = TRUE
+  )
+  if (!dir.exists(tmp_dir2)) dir.create(tmp_dir2)
+  expect_error(
+    check_rtools35_windows_toolchain(paths= c(tmp_dir1, tmp_dir2)),
+    "\nMultiple RTools 3.5 installations found. Please select the installation to use",
+    fixed = TRUE
+  )
+  Sys.setenv("PATH" = path_backup)
+})
 
 test_that("toolchain checks without fixes on Windows with RTools 4.0 work", {
   skip_if_not(os_is_windows())
+  skip_if(R.Version()$major < "4")
+
   rtools40_home_backup <- Sys.getenv("RTOOLS40_HOME")
   Sys.setenv("RTOOLS40_HOME" = "")
   expect_error(
