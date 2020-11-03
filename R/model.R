5fe1fa9666cd2950ee61fccfdf0b633d6660c531
Mike Lawrence <mike@axemneuro.com>
2020-11-03 13:14:14 -0400

    finally working

--- R/model.R
+++ R/model.R
@@ -578,7 +578,7 @@ check_syntax_method <- function(quiet = FALSE,
     } else {
       include_paths_flag <- " --include_paths="
     }
-    stancflags_val <- trimws(paste0(stancflags_val, include_paths_flag, include_paths, " "))
+    stancflags_val <- trimws(paste0(include_paths_flag, include_paths, " "))
   }
 
   if (is.null(stanc_options[["name"]])) {
@@ -595,10 +595,15 @@ check_syntax_method <- function(quiet = FALSE,
     }
   }
 
+  if(stancflags_val==''){
+    these_args = c(self$stan_file(), stanc_built_options)
+  }else{
+    these_args = c(self$stan_file(), stanc_built_options, stancflags_val)
+  }
 
   run_log <- processx::run(
     command = stanc_cmd(),
-    args = c(self$stan_file(), stanc_built_options, stancflags_val),
+    args = these_args,
     wd = cmdstan_path(),
     echo_cmd = !quiet,
     echo = !quiet,
