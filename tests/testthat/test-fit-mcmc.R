8444ca65f7e0b3bc8d334e12424365347845f418
jgabry <jgabry@gmail.com>
2020-12-04 10:48:12 -0700

    in tests only, suppress warnings from loo about pareto k values

--- tests/testthat/test-fit-mcmc.R
+++ tests/testthat/test-fit-mcmc.R
@@ -259,8 +259,8 @@ test_that("loo method works if log_lik is available", {
   skip_on_cran()
   skip_if_not_installed("loo")
   fit_bernoulli <- testing_fit("bernoulli_log_lik")
-  expect_s3_class(fit_bernoulli$loo(cores = 2, save_psis = TRUE), "loo")
-  expect_s3_class(fit_bernoulli$loo(r_eff = FALSE), "loo")
+  expect_s3_class(suppressWarnings(fit_bernoulli$loo(cores = 2, save_psis = TRUE)), "loo")
+  expect_s3_class(suppressWarnings(fit_bernoulli$loo(r_eff = FALSE)), "loo")
 })
 
 test_that("loo errors if it can't find log lik variables", {
