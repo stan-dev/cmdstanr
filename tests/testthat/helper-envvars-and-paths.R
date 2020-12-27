0194df4c6b477246fc1293f3416b403b6a18d4a3
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-12-27 10:31:47 +0100

    Added automatic check if mpi toolchain is present. If not we skip sample_mpi tests.

--- tests/testthat/helper-envvars-and-paths.R
+++ tests/testthat/helper-envvars-and-paths.R
@@ -10,6 +10,16 @@ not_on_cran <- function() {
   on_ci() || identical(Sys.getenv("NOT_CRAN"), "true")
 }
 
+mpi_toolchain_present <- function() {
+  tryCatch(
+    processx::run(command = "mpicxx", args = "--version")$status == 0 &&
+    processx::run(command = "mpiexec", args = "--version")$status == 0,
+    error=function(cond) {
+      FALSE
+    }
+  )
+}
+
 delete_extensions <- function() {
   if (os_is_windows()) {
     c(".exe", ".o", ".hpp")
--- tests/testthat/test-model-sample_mpi.R
+++ tests/testthat/test-model-sample_mpi.R
@@ -2,7 +2,7 @@ context("model-sample_mpi")
 
 test_that("sample_mpi() works", {
   skip_on_cran()
-  skip_if(!nzchar(Sys.getenv("CMDSTANR_RUN_MPI_TESTS")))
+  skip_if(!mpi_toolchain_present())
   mpi_file <- write_stan_file("
   functions {
     vector test(vector beta, vector theta, real[] x, int[] y) {
@@ -25,7 +25,12 @@ test_that("sample_mpi() works", {
     vector[20] c = map_rect(test, a, b, x, y);
   }
   ")
-  cpp_options = list(cxx="mpicxx", stan_mpi = TRUE, tbb_cxx_type="gcc")
+  if (os_is_macos()) {
+    tbb_cxx_type <- "clang"
+  } else {
+    tbb_cxx_type <- "gcc"
+  }
+  cpp_options = list(cxx="mpicxx", stan_mpi = TRUE, tbb_cxx_type=tbb_cxx_type)
   mod_mpi <- cmdstan_model(mpi_file, cpp_options = cpp_options)
   utils::capture.output(
     f <- mod_mpi$sample_mpi(chains = 1, mpi_args = list("n" = 1))
