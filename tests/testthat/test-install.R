4cc51af8d2441e27b9090173ad2261eb30d81d5b
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-23 19:53:18 +0100

    dont run install test that relies on latest release not being RC

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -30,6 +30,7 @@ test_that("install_cmdstan() successfully installs cmdstan", {
 test_that("install_cmdstan() errors if installation already exists", {
   skip_if_offline()
   skip_on_cran()
+  skip_if(grepl("rc.+$", cmdstanr::cmdstan_path()))
   if (not_on_cran()) {
     # want to test passing NULL to install_cmdstan but need a real dir to
     # check in dir.exists() below so also create dir_check
