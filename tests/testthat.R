b357809563f0ef1cd1bf851726aa0a9065a18e10
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-10-24 19:35:17 +0300

    fit-shared debugging

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,5 @@ library(cmdstanr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   #test_check("cmdstanr")
-  test_file("tests/testthat/test-fit-shared.R")
+  test_file("testthat/test-fit-shared.R")
 }
