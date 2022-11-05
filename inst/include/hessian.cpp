8d0b8a082f7e346b5f1d605a3873e543a618bd00
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-11-05 21:12:13 +0200

    Testing adj option

--- inst/include/hessian.cpp
+++ inst/include/hessian.cpp
@@ -1,17 +1,28 @@
 #include <RcppEigen.h>
-#include <stan/model/hessian.hpp>
+#include <stan/math/mix/functor/hessian.hpp>
 
 // [[Rcpp::depends(RcppEigen)]]
 
 // [[Rcpp::export]]
-Rcpp::List hessian(SEXP ext_model_ptr, Eigen::VectorXd& upars) {
+Rcpp::List hessian(SEXP ext_model_ptr, Eigen::VectorXd& upars,
+                   bool jac_adjust) {
   Rcpp::XPtr<stan_model> ptr(ext_model_ptr);
+
+  auto log_prob_fun = [](const bool adjust, Rcpp::XPtr<stan_model> model_ptr) {
+    return [=](auto& pars) {
+      if (adjust) {
+        return model_ptr.get()->log_prob<true, true>(pars, &Rcpp::Rcout);
+      } else {
+        return model_ptr.get()->log_prob<true, false>(pars, &Rcpp::Rcout);
+      }
+    };
+  };
 
   double log_prob;
   Eigen::VectorXd grad;
   Eigen::MatrixXd hessian;
 
-  stan::model::hessian(*ptr.get(), upars, log_prob, grad, hessian);
+  stan::math::hessian(log_prob_fun(jac_adjust, ptr), upars, log_prob, grad, hessian);
 
   return Rcpp::List::create(
     Rcpp::Named("log_prob") = log_prob,
