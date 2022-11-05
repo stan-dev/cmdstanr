18b5d7e141914dda7bea27b89d49f27acd1bf4ae
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-11-05 22:05:35 +0200

    Add jac adj option to hessian

--- inst/include/hessian.cpp
+++ inst/include/hessian.cpp
@@ -1,28 +1,40 @@
 #include <RcppEigen.h>
-#include <stan/math/mix/functor/hessian.hpp>
+#include <stan/model/hessian.hpp>
 
 // [[Rcpp::depends(RcppEigen)]]
 
-// [[Rcpp::export]]
-Rcpp::List hessian(SEXP ext_model_ptr, Eigen::VectorXd& upars,
-                   bool jac_adjust) {
-  Rcpp::XPtr<stan_model> ptr(ext_model_ptr);
+template <class M>
+struct hessian_wrapper {
+  const M& model;
+  bool jac_adjust;
+  std::ostream* o;
 
-  auto log_prob_fun = [](const bool adjust, Rcpp::XPtr<stan_model> model_ptr) {
-    return [=](auto& pars) {
-      if (adjust) {
-        return model_ptr.get()->log_prob<true, true>(pars, &Rcpp::Rcout);
-      } else {
-        return model_ptr.get()->log_prob<true, false>(pars, &Rcpp::Rcout);
-      }
-    };
-  };
+  hessian_wrapper(const M& m, bool adj, std::ostream* out) : model(m), jac_adjust(adj), o(out) {}
+
+  template <typename T>
+  T operator()(const Eigen::Matrix<T, Eigen::Dynamic, 1>& x) const {
+    if (jac_adjust) {
+      // log_prob() requires non-const but doesn't modify its argument
+      return model.template log_prob<true, true, T>(
+          const_cast<Eigen::Matrix<T, -1, 1>&>(x), o);
+    } else {
+      // log_prob() requires non-const but doesn't modify its argument
+      return model.template log_prob<true, false, T>(
+          const_cast<Eigen::Matrix<T, -1, 1>&>(x), o);
+    }
+  }
+};
+
+// [[Rcpp::export]]
+Rcpp::List hessian(SEXP ext_model_ptr, Eigen::VectorXd& upars, bool jac_adjust) {
+  Rcpp::XPtr<stan_model> ptr(ext_model_ptr);
 
   double log_prob;
   Eigen::VectorXd grad;
   Eigen::MatrixXd hessian;
 
-  stan::math::hessian(log_prob_fun(jac_adjust, ptr), upars, log_prob, grad, hessian);
+  stan::math::hessian(hessian_wrapper<decltype(*ptr.get())>(*ptr.get(), jac_adjust, 0),
+                      upars, log_prob, grad, hessian);
 
   return Rcpp::List::create(
     Rcpp::Named("log_prob") = log_prob,
