94f6395a0c83c6d13abe480e0d6ff28ab088d08c
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-04-05 11:00:42 +0300

    Fix Rtools42 minor version

--- R/utils.R
+++ R/utils.R
@@ -55,7 +55,7 @@ is_rtools43_toolchain <- function() {
 }
 
 is_rtools42_toolchain <- function() {
-  os_is_windows() && R.version$major == "4" && R.version$minor == "2.0"
+  os_is_windows() && R.version$major == "4" && R.version$minor >= "2.0" && R.version$minor < "3.0"
 }
 
 is_rtools40_toolchain <- function() {
