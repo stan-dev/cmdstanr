e71126553bc217f342702a2847beb008ea3ebeac
Steve Bronder <sbronder@flatironinstitute.org>
2024-05-03 15:31:02 -0400

    remove set.seed() in each init test

--- tests/testthat/test-fit-init.R
+++ tests/testthat/test-fit-init.R
@@ -5,34 +5,25 @@ set_cmdstan_path()
 data_list_schools <- testing_data("schools")
 data_list_logistic <- testing_data("logistic")
 test_inits <- function(mod, fit_init, data_list = NULL) {
-  Sys.sleep(1)
   fit_sample <- mod$sample(data = data_list, chains = 1, init = fit_init,
     iter_sampling = 100, iter_warmup = 100, refresh = 0, seed = 1234)
-  Sys.sleep(1)
   fit_sample_multi <- mod$sample(data = data_list, chains = 5, init = fit_init,
     iter_sampling = 100, iter_warmup = 100, refresh = 0, seed = 1234)
-  Sys.sleep(1)
   fit_vb <- mod$variational(data = data_list, refresh = 0, seed = 1234,
     init = fit_init, algorithm = "fullrank")
-  Sys.sleep(1)
   fit_path <- mod$pathfinder(data = data_list, seed=1234, refresh = 0,
     num_paths = 4, init = fit_init)
-  Sys.sleep(1)
   fit_laplace <- mod$laplace(data = data_list, seed = 1234, refresh=0,
     init=fit_init)
-  Sys.sleep(1)
   fit_ml <- mod$optimize(data = data_list, seed = 1234, refresh = 0,
     init = fit_init, history_size = 400, algorithm = "lbfgs")
-  Sys.sleep(1)
   draws = posterior::as_draws_rvars(fit_init$draws())
   fit_sample_draws <- mod$sample(data = data_list, chains = 1, init = draws,
     iter_sampling = 100, iter_warmup = 100, refresh = 0, seed = 1234)
-  Sys.sleep(1)
   return(0)
 }
 
 test_that("Sample method works as init", {
-  set.seed(1234)
   mod_params <- testing_model("parameter_types")
   utils::capture.output(fit_sample_init <- mod_params$sample(chains = 1,
     iter_warmup = 100, iter_sampling = 100, refresh = 0, seed = 1234))
@@ -40,7 +31,6 @@ test_that("Sample method works as init", {
 })
 
 test_that("Multi chain Sample method works as init", {
-  set.seed(1234)
   mod_params <- testing_model("parameter_types")
   utils::capture.output(fit_sample_multi_init <- mod_params$sample(chains = 4,
     iter_warmup = 100, iter_sampling = 100, refresh = 0, seed = 1234))
@@ -48,7 +38,6 @@ test_that("Multi chain Sample method works as init", {
 })
 
 test_that("Subsets of parameters are allowed", {
-  set.seed(1234)
   mod_logistic_simple <- testing_model("logistic_simple")
   utils::capture.output(fit_sample_init_simple <- mod_logistic_simple$sample(chains = 1,
     data = data_list_logistic, iter_warmup = 100, iter_sampling = 100,
@@ -66,7 +55,6 @@ test_that("Pathfinder works as init", {
 })
 
 test_that("Multi Pathfinder method works as init", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_path_init <- mod_logistic$pathfinder(seed=1234,
     data = data_list_logistic, refresh = 0, num_paths = 4))
@@ -74,8 +62,6 @@ test_that("Multi Pathfinder method works as init", {
 })
 
 test_that("Pathfinder method with psis_resample as false works as init", {
-  set.seed(1234)
-  Sys.sleep(3)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_path_init <- mod_logistic$pathfinder(
     seed=1234, data = data_list_logistic, refresh = 0, num_paths = 1))
@@ -84,7 +70,6 @@ test_that("Pathfinder method with psis_resample as false works as init", {
 
 
 test_that("Multi Pathfinder method with psis_resample as false works as init", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_path_init <- mod_logistic$pathfinder(
     seed=1234, data = data_list_logistic, refresh = 0, num_paths = 4,
@@ -94,7 +79,6 @@ test_that("Multi Pathfinder method with psis_resample as false works as init", {
 
 
 test_that("Pathfinder method with calculate_lp as false works as init", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   fit_path_init <- mod_logistic$pathfinder(
     seed=1234, data = data_list_logistic, refresh = 0, num_paths = 1,
@@ -103,7 +87,6 @@ test_that("Pathfinder method with calculate_lp as false works as init", {
 })
 
 test_that("Multi Pathfinder method with calculate_lp as false works as init", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_path_init <- mod_logistic$pathfinder(
     seed=1234, data = data_list_logistic, refresh = 0, num_paths = 4,
@@ -112,7 +95,6 @@ test_that("Multi Pathfinder method with calculate_lp as false works as init", {
 })
 
 test_that("Variational method works as init", {
-  set.seed(1235)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_vb_init <- mod_logistic$variational(
     data = data_list_logistic, seed=1234, refresh = 0))
@@ -120,7 +102,6 @@ test_that("Variational method works as init", {
 })
 
 test_that("Optimization method works as init", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_ml_init <- mod_logistic$optimize(
     data = data_list_logistic, seed=1234, refresh = 0))
@@ -129,7 +110,6 @@ test_that("Optimization method works as init", {
 
 
 test_that("Draws Object with NA or Inf throws error", {
-  set.seed(1234)
   mod_logistic <- testing_model("logistic")
   utils::capture.output(fit_laplace_init <- mod_logistic$laplace(
     data = data_list_logistic, seed = 1234, refresh=0))
