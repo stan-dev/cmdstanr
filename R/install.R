11853aee4cbfbc229500de8565dd37768c5195f9
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-01-04 21:44:14 +0800

    Typo

--- R/install.R
+++ R/install.R
@@ -103,7 +103,7 @@ install_cmdstan <- function(dir = NULL,
   } else {
     .cmdstanr$WSL <- FALSE
   }
-  if (os_is_windows() && !os_is_wsl() && version < "2.35.0") {
+  if (os_is_windows() && !os_is_wsl() && isTRUE(version < "2.35.0")) {
     if (Sys.getenv("CMDSTANR_USE_MSYS_TOOLCHAIN") == "") {
       stop("CmdStan versions prior to 2.35.0 require additional toolchain configuration on Windows.\n",
             "Please set the environment variable CMDSTANR_USE_MSYS_TOOLCHAIN to 'true' and \n",
