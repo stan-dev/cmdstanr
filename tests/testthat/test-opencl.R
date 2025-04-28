32260b4d27ccb28bd18928a5a2e3d7403df97013
Brock <kbrock@ab.mpg.de>
2025-04-28 12:53:55 +0200

    update opencl tests

--- tests/testthat/test-opencl.R
+++ tests/testthat/test-opencl.R
@@ -5,7 +5,7 @@ fit <- testing_fit("bernoulli", method = "sample", seed = 123, chains = 1)
 
 test_that("all methods error when opencl_ids is used with non OpenCL model", {
   stan_file <- testing_stan_file("bernoulli")
-  mod <- cmdstan_model(stan_file = stan_file)
+  mod <- cmdstan_model(stan_file = stan_file, force_recompile = TRUE)
   expect_error(
     mod$sample(data = testing_data("bernoulli"), opencl_ids = c(0, 0), chains = 1),
     "'opencl_ids' is set but the model was not compiled with for use with OpenCL.",
@@ -22,7 +22,7 @@ test_that("all methods error when opencl_ids is used with non OpenCL model", {
     fixed = TRUE
   )
   stan_file_gq <- testing_stan_file("bernoulli_ppc")
-  mod_gq <- cmdstan_model(stan_file = stan_file_gq)
+  mod_gq <- cmdstan_model(stan_file = stan_file_gq, force_recompile = TRUE)
   expect_error(
     mod_gq$generate_quantities(fitted_params = fit, data = testing_data("bernoulli"), opencl_ids = c(0, 0)),
     "'opencl_ids' is set but the model was not compiled with for use with OpenCL.",
@@ -33,8 +33,11 @@ test_that("all methods error when opencl_ids is used with non OpenCL model", {
 test_that("all methods error on invalid opencl_ids", {
   skip_if_not(Sys.getenv("CMDSTANR_OPENCL_TESTS") %in% c("1", "true"))
   stan_file <- testing_stan_file("bernoulli")
-  mod <- cmdstan_model(stan_file = stan_file, cpp_options = list(stan_opencl = TRUE),
-                       force_recompile = TRUE)
+  mod <- cmdstan_model(
+    stan_file = stan_file,
+    force_recompile = TRUE,
+    cpp_options = list(stan_opencl = TRUE)
+  )
   utils::capture.output(
     expect_warning(
       mod$sample(data = testing_data("bernoulli"), opencl_ids = c(1000, 1000), chains = 1),
@@ -57,7 +60,11 @@ test_that("all methods error on invalid opencl_ids", {
     )
   )
   stan_file_gq <- testing_stan_file("bernoulli_ppc")
-  mod_gq <- cmdstan_model(stan_file = stan_file_gq, cpp_options = list(stan_opencl = TRUE))
+  mod_gq <- cmdstan_model(
+    stan_file = stan_file_gq,
+    force_recompile = TRUE,
+    cpp_options = list(stan_opencl = TRUE)
+  )
   utils::capture.output(
     expect_warning(
       mod_gq$generate_quantities(fitted_params = fit, data = testing_data("bernoulli"), opencl_ids = c(1000, 1000)),
@@ -70,39 +77,56 @@ test_that("all methods error on invalid opencl_ids", {
 test_that("all methods run with valid opencl_ids", {
   skip_if_not(Sys.getenv("CMDSTANR_OPENCL_TESTS") %in% c("1", "true"))
   stan_file <- testing_stan_file("bernoulli")
-  mod <- cmdstan_model(stan_file = stan_file, cpp_options = list(stan_opencl = TRUE))
+  mod <- cmdstan_model(
+    stan_file = stan_file,
+    force_recompile = TRUE,
+    cpp_options = list(stan_opencl = TRUE)
+  )
   expect_sample_output(
     fit <- mod$sample(data = testing_data("bernoulli"), opencl_ids = c(0, 0), chains = 1)
   )
   expect_false(is.null(fit$metadata()$opencl_platform_name))
   expect_false(is.null(fit$metadata()$opencl_device_name))
+  expect_false(is.null(fit$metadata()$device))
+  expect_false(is.null(fit$metadata()$platform))
 
   stan_file_gq <- testing_stan_file("bernoulli_ppc")
-  mod_gq <- cmdstan_model(stan_file = stan_file_gq, cpp_options = list(stan_opencl = TRUE),
-                          force_recompile = TRUE)
+  mod_gq <- cmdstan_model(
+    stan_file = stan_file_gq,
+    force_recompile = TRUE,
+    cpp_options = list(stan_opencl = TRUE)
+  )
   expect_gq_output(
     fit <- mod_gq$generate_quantities(fitted_params = fit, data = testing_data("bernoulli"), opencl_ids = c(0, 0)),
   )
   expect_false(is.null(fit$metadata()$opencl_platform_name))
   expect_false(is.null(fit$metadata()$opencl_device_name))
+  expect_false(is.null(fit$metadata()$device))
+  expect_false(is.null(fit$metadata()$platform))
 
   expect_sample_output(
     fit <- mod$sample(data = testing_data("bernoulli"), opencl_ids = c(0, 0))
   )
   expect_false(is.null(fit$metadata()$opencl_platform_name))
   expect_false(is.null(fit$metadata()$opencl_device_name))
+  expect_false(is.null(fit$metadata()$device))
+  expect_false(is.null(fit$metadata()$platform))
 
   expect_optim_output(
     fit <- mod$optimize(data = testing_data("bernoulli"), opencl_ids = c(0, 0))
   )
   expect_false(is.null(fit$metadata()$opencl_platform_name))
   expect_false(is.null(fit$metadata()$opencl_device_name))
+  expect_false(is.null(fit$metadata()$device))
+  expect_false(is.null(fit$metadata()$platform))
 
   expect_vb_output(
     fit <- mod$variational(data = testing_data("bernoulli"), opencl_ids = c(0, 0))
   )
   expect_false(is.null(fit$metadata()$opencl_platform_name))
   expect_false(is.null(fit$metadata()$opencl_device_name))
+  expect_false(is.null(fit$metadata()$device))
+  expect_false(is.null(fit$metadata()$platform))
 })
 
 test_that("error for runtime selection of OpenCL devices if version less than 2.26", {
@@ -113,8 +137,16 @@ test_that("error for runtime selection of OpenCL devices if version less than 2.
                        force_recompile = TRUE)
   fake_cmdstan_version("2.25.0", mod)
   expect_error(
-    mod$sample(data = testing_data("bernoulli"), chains = 1, refresh = 0, opencl_ids = c(1,1)),
-    "Runtime selection of OpenCL devices is only supported with CmdStan version 2.26 or newer",
+    mod$sample(
+      data = testing_data("bernoulli"),
+      chains = 1,
+      refresh = 0,
+      opencl_ids = c(0, 0)
+    ),
+    paste0(
+      "Runtime selection of OpenCL devices ",
+      "is only supported with CmdStan version 2.26 or newer"
+    ),
     fixed = TRUE
   )
   reset_cmdstan_version(mod)
