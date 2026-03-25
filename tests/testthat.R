8d926bb72ebcd7daeacf6eae526e43cbe82a8acf
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-03-24 21:41:33 -0700

    Run stateful tests sequentially

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   withr::local_options(list(mc.cores = 1L))
   withr::local_envvar(TESTTHAT_IS_CHECKING = "true")
 
-  sequential_files <- "example|model-compile|model-compile-user_header|model-methods|utils"
+  sequential_files <- "example|install|model-compile|model-compile-user_header|model-methods|path|utils"
 
   withr::with_envvar(
     c(TESTTHAT_PARALLEL = "false"),
