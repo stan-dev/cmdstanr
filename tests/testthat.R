1366910cc2506848a994a1a641f77756bc624bc0
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-03-25 06:00:10 -0700

    Simplify test harness

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,9 +2,7 @@ library(testthat)
 library(cmdstanr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  options(cli.hyperlink = FALSE)
   withr::local_options(list(mc.cores = 1L))
-  withr::local_envvar(TESTTHAT_IS_CHECKING = "true")
 
   sequential_files <- "example|install|model-compile|model-compile-user_header|model-methods|opencl|path|threads|utils"
 
@@ -17,13 +15,10 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
              load_package = "installed")
   )
 
-  withr::with_envvar(
-    c(TESTTHAT_PARALLEL = "true"),
-    test_dir("testthat",
-             package = "cmdstanr",
-             reporter = check_reporter(),
-             filter = sequential_files,
-             invert = TRUE,
-             load_package = "installed")
-  )
+  test_dir("testthat",
+           package = "cmdstanr",
+           reporter = check_reporter(),
+           filter = sequential_files,
+           invert = TRUE,
+           load_package = "installed")
 }
