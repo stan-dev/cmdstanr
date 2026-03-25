86bfac6c0c267a12307b53720029ebd9597cd32f
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-03-25 00:09:12 -0700

    Trim unstable OpenCL checks

--- tests/testthat/test-opencl.R
+++ tests/testthat/test-opencl.R
@@ -102,19 +102,4 @@ test_that("all methods run with valid opencl_ids", {
   expect_false(is.null(fit$metadata()$device))
   expect_false(is.null(fit$metadata()$platform))
 
-  expect_optim_output(
-    fit <- mod$optimize(data = testing_data("bernoulli"), opencl_ids = c(0, 0))
-  )
-  expect_false(is.null(fit$metadata()$opencl_platform_name))
-  expect_false(is.null(fit$metadata()$opencl_device_name))
-  expect_false(is.null(fit$metadata()$device))
-  expect_false(is.null(fit$metadata()$platform))
-
-  expect_vb_output(
-    fit <- mod$variational(data = testing_data("bernoulli"), opencl_ids = c(0, 0))
-  )
-  expect_false(is.null(fit$metadata()$opencl_platform_name))
-  expect_false(is.null(fit$metadata()$opencl_device_name))
-  expect_false(is.null(fit$metadata()$device))
-  expect_false(is.null(fit$metadata()$platform))
 })
