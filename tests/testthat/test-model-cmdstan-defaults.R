e64c6569168b40910d9465e3c53f1bfd12cb2cbc
Aki Vehtari <Aki.Vehtari@aalto.fi>
2026-03-31 21:46:38 +0300

    test that cmdstan defaults match the current known values

--- tests/testthat/test-model-cmdstan-defaults.R
+++ tests/testthat/test-model-cmdstan-defaults.R
@@ -85,6 +85,79 @@ test_that("cmdstan_defaults() works for laplace", {
   expect_type(args$draws, "integer")
 })
 
+# default values ----------------------------------------------------------
+
+test_that("cmdstan_defaults() returns expected default values for sample", {
+  args <- mod$cmdstan_defaults("sample")
+  expect_identical(args$iter_sampling, 1000L)
+  expect_identical(args$iter_warmup, 1000L)
+  expect_identical(args$save_warmup, FALSE)
+  expect_identical(args$thin, 1L)
+  expect_identical(args$adapt_engaged, TRUE)
+  expect_identical(args$adapt_delta, 0.8)
+  expect_identical(args$init_buffer, 75L)
+  expect_identical(args$term_buffer, 50L)
+  expect_identical(args$window, 25L)
+  expect_identical(args$save_metric, FALSE)
+  expect_identical(args$max_treedepth, 10L)
+  expect_identical(args$metric, "diag_e")
+  expect_identical(args$step_size, 1L)
+  expect_identical(args$chains, 1L)
+})
+
+test_that("cmdstan_defaults() returns expected default values for optimize", {
+  args <- mod$cmdstan_defaults("optimize")
+  expect_identical(args$algorithm, "lbfgs")
+  expect_identical(args$jacobian, FALSE)
+  expect_identical(args$iter, 2000L)
+  expect_identical(args$init_alpha, 0.001)
+  expect_identical(args$tol_obj, 1e-12)
+  expect_identical(args$tol_rel_obj, 10000L)
+  expect_identical(args$tol_grad, 1e-08)
+  expect_identical(args$tol_rel_grad, 1e+07)
+  expect_identical(args$tol_param, 1e-08)
+  expect_identical(args$history_size, 5L)
+})
+
+test_that("cmdstan_defaults() returns expected default values for variational", {
+  args <- mod$cmdstan_defaults("variational")
+  expect_identical(args$algorithm, "meanfield")
+  expect_identical(args$iter, 10000L)
+  expect_identical(args$grad_samples, 1L)
+  expect_identical(args$elbo_samples, 100L)
+  expect_identical(args$eta, 1L)
+  expect_identical(args$adapt_engaged, TRUE)
+  expect_identical(args$adapt_iter, 50L)
+  expect_identical(args$tol_rel_obj, 0.01)
+  expect_identical(args$eval_elbo, 100L)
+  expect_identical(args$draws, 1000L)
+})
+
+test_that("cmdstan_defaults() returns expected default values for pathfinder", {
+  args <- mod$cmdstan_defaults("pathfinder")
+  expect_identical(args$init_alpha, 0.001)
+  expect_identical(args$tol_obj, 1e-12)
+  expect_identical(args$tol_rel_obj, 10000L)
+  expect_identical(args$tol_grad, 1e-08)
+  expect_identical(args$tol_rel_grad, 1e+07)
+  expect_identical(args$tol_param, 1e-08)
+  expect_identical(args$history_size, 5L)
+  expect_identical(args$draws, 1000L)
+  expect_identical(args$num_paths, 4L)
+  expect_identical(args$save_single_paths, FALSE)
+  expect_identical(args$psis_resample, TRUE)
+  expect_identical(args$calculate_lp, TRUE)
+  expect_identical(args$max_lbfgs_iters, 1000L)
+  expect_identical(args$single_path_draws, 1000L)
+  expect_identical(args$num_elbo_draws, 25L)
+})
+
+test_that("cmdstan_defaults() returns expected default values for laplace", {
+  args <- mod$cmdstan_defaults("laplace")
+  expect_identical(args$jacobian, TRUE)
+  expect_identical(args$draws, 1000L)
+})
+
 # internal helpers --------------------------------------------------------
 
 test_that("parse_default_value() parses booleans", {
