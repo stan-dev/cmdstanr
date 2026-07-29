9b062c2b08ec24a5fe8422cee396b82d910c9789
jgabry <jgabry@gmail.com>
2026-07-29 15:40:50 -0600

    remove confusing references to MAP and MLE
    closes #1239

--- R/fit.R
+++ R/fit.R
@@ -1972,14 +1972,12 @@ CmdStanMCMC$set("public", name = "num_chains", value = num_chains)
 #'
 #' @description A `CmdStanMLE` object is the fitted model object returned by the
 #'   [`$optimize()`][model-method-optimize] method of a [`CmdStanModel`] object.
-#'   Following CmdStan's terminology, the object contains an MLE if optimization
-#'   was run with `jacobian=FALSE` and a MAP estimate if it was run with
-#'   `jacobian=TRUE`. The name "MLE" is retained for historical reasons. More
-#'   precisely, the estimates correspond to a mode in either the constrained
-#'   parameter space or the unconstrained parameter space, depending on the
-#'   value of `jacobian` (and whether the model has constrained parameters).
-#'   The `jacobian` argument does not control whether prior terms are included;
-#'   all contributions to the Stan program's target are included under either
+#'   The name "MLE" is retained for historical reasons. With
+#'   `jacobian = FALSE`, the point estimate is a mode of the target in the
+#'   constrained parameter space. With `jacobian = TRUE`, it is a mode of the
+#'   corresponding density in the unconstrained parameter space. The `jacobian`
+#'   argument does not control whether prior terms are included; all
+#'   contributions to the Stan program's target are included under either
 #'   setting. See [`$optimize()`][model-method-optimize] and the CmdStan User's
 #'   Guide for more details.
 #'
@@ -2069,19 +2067,18 @@ CmdStanMLE <- R6::R6Class(
 #' @name fit-method-mle
 #' @aliases mle
 #' @description The `$mle()` method is only available for [`CmdStanMLE`]
-#'   objects. It returns the point estimate as a numeric vector with one element
-#'   per variable. The returned vector does *not* include `lp__`, the
-#'   target log density evaluated by Stan, up to an additive constant. `lp__` is
-#'   available via the [`$lp()`][fit-method-lp] method and also included in the
+#'   objects. The method name is retained for historical reasons. It returns the
+#'   point estimate as a numeric vector with one element per variable. The
+#'   returned vector does *not* include `lp__`, the target log density evaluated
+#'   by Stan, up to an additive constant. `lp__` is available via the
+#'   [`$lp()`][fit-method-lp] method and also included in the
 #'   [`$draws()`][fit-method-draws] method.
 #'
-#'   Following CmdStan's terminology, for models with constrained parameters
-#'   that are fit with `jacobian=TRUE`, this point estimate is called a maximum
-#'   a posteriori (MAP) estimate rather than an MLE. More precisely,
-#'   `jacobian=FALSE` finds a mode of the target in the constrained parameter
-#'   space and `jacobian=TRUE` finds a mode in the unconstrained parameter
-#'   space. See [`$optimize()`][model-method-optimize] and the CmdStan User's
-#'   Guide for more details.
+#'   With `jacobian = FALSE`, the point estimate is a mode of the target in the
+#'   constrained parameter space. With `jacobian = TRUE`, it is a mode of the
+#'   corresponding density in the unconstrained parameter space. See
+#'   [`$optimize()`][model-method-optimize] and the CmdStan User's Guide for more
+#'   details.
 #'
 #' @param variables (character vector) The variables (parameters, transformed
 #'   parameters, and generated quantities) to include. If NULL (the default)
